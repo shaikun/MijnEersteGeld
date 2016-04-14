8a9337b3a853ec1f952b4cdac55b85a6b944bbf4
Niels van Oijen <Shaikun@macbook-pro-3.local>
2016-04-14 13:46:42 +0200

    Initial Commit

---
+++ MijnEersteGeld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MijnEersteGeld
+//
+//  Created by Niels van Oijen on 14-04-16.
+//  Copyright © 2016 Niels van Oijen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
