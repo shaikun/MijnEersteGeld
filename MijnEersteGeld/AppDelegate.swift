634c682d49a36d773e7f483175ecc3b499370ee1
Niels van Oijen <Shaikun@macbook-pro-3.local>
2016-04-15 13:27:59 +0200

    Initial commit

--- MijnEersteGeld/AppDelegate.swift
+++ MijnEersteGeld/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        
+        //todo perform segway with identiefier
         return true
     }
 
